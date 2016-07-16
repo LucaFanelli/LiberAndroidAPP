b9f9241f941bd8945704dc885104366f0ce197c1
luca <luca.luca.fanelli@gmail.com>
2016-07-16 17:33:58 +0200

    first

---
+++ mobile/src/main/java/it/app/liber/liber/LoginActivity.java
@@ -0,0 +1,22 @@
+package it.app.liber.liber;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void login(View view) {
+        Intent intent = new Intent(this, HomeActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+    }
+
+}
